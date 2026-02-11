6ec6469d24177dc96f5b9e1ae82e54eaa2b6311b
AllisonJiao <allisonjiao02@gmail.com>
2026-02-10 16:51:10 -0800

    Fix API call

--- mjlab/tasks/velocity/config/g1/scene_cfg.py
+++ mjlab/tasks/velocity/config/g1/scene_cfg.py
@@ -1,26 +1,23 @@
 """Unitree G1 scene configuration."""
-from dataclasses import replace
 
 from mjlab.scene import SceneCfg
 from mjlab.asset_zoo.robots.unitree_g1.g1_constants import get_g1_robot_cfg
-from mjlab.utils.spec_config import ContactSensorCfg
+from mjlab.sensor import ContactMatch, ContactSensorCfg
 from mjlab.terrains import TerrainImporterCfg
 
 # Configure contact sensor
 self_collision_sensor = ContactSensorCfg(
     name="self_collision",
-    subtree1="pelvis",
-    subtree2="pelvis",
-    data=("found",),
-    reduce="netforce",
-    num=10,  # report up to 10 contacts
+    primary=ContactMatch(mode="subtree", pattern="pelvis", entity="robot"),
+    secondary=ContactMatch(mode="subtree", pattern="pelvis", entity="robot"),
+    fields=("found",),
+    reduce="none",
+    num_slots=1,
 )
-
-# Add sensor to robot config
-g1_cfg = replace(get_g1_robot_cfg(), sensors=(self_collision_sensor,))
 
 # Create scene
 SCENE_CFG = SceneCfg(
     terrain=TerrainImporterCfg(terrain_type="plane"),
-    entities={"robot": g1_cfg},
+    entities={"robot": get_g1_robot_cfg()},
+    sensors=(self_collision_sensor,),
 )
