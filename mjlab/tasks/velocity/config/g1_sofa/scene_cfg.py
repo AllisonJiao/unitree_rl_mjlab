10ef387fa2779aabfd27edc7d187dd017e6e29bd
AllisonJiao <allisonjiao02@gmail.com>
2026-02-10 18:51:18 -0800

    Fix sofa asset

--- mjlab/tasks/velocity/config/g1_sofa/scene_cfg.py
+++ mjlab/tasks/velocity/config/g1_sofa/scene_cfg.py
@@ -4,7 +4,6 @@ import mujoco
 
 from mjlab import MJLAB_SRC_PATH
 from mjlab.asset_zoo.robots import get_g1_robot_cfg
-from mjlab.entity import EntityCfg
 from mjlab.scene import SceneCfg
 from mjlab.sensor import ContactMatch, ContactSensorCfg
 from mjlab.terrains import TerrainImporterCfg
@@ -16,20 +15,27 @@ def create_scene_cfg() -> SceneCfg:
     Returns:
         SceneCfg instance with sofa and two robots
     """
-    # Path to the sofa asset
-    sofa_path = MJLAB_SRC_PATH / "asset_zoo" / "objects" / "sofa" / "sofa.usdc"
+    # Path to the sofa XML file
+    sofa_xml_path = MJLAB_SRC_PATH / "asset_zoo" / "objects" / "sofa" / "sofa.xml"
     
-    # Create EntityCfg for the sofa
-    def sofa_spec_fn() -> mujoco.MjSpec:
-        """Load the sofa USD file as a MuJoCo spec."""
-        return mujoco.MjSpec.from_file(str(sofa_path))
-    
-    sofa_cfg = EntityCfg(
-        spec_fn=sofa_spec_fn,
-        init_state=EntityCfg.InitialStateCfg(
-            pos=(2.0, 0.0, 0.0),  # Position the sofa 2 meters to the right
-        ),
-    )
+    def add_sofa_asset(spec: mujoco.MjSpec) -> None:
+        """Add the sofa XML asset to the scene using spec_fn.
+        
+        This is called AFTER sensors are added, so we need to be careful
+        not to cause conflicts.
+        """
+        # Load the sofa XML as a separate spec
+        sofa_spec = mujoco.MjSpec.from_file(str(sofa_xml_path))
+        
+        # Clear any sensors from sofa_spec to avoid conflicts
+        for sensor in list(sofa_spec.sensors):
+            sofa_spec.delete(sensor)
+        
+        # Attach the sofa spec to the scene's worldbody
+        frame = spec.worldbody.add_frame()
+        # Position the sofa 2 meters to the right
+        frame.pos = [2.0, 0.0, 0.0]
+        spec.attach(sofa_spec, prefix="sofa/", frame=frame)
     
     # Get G1 robot config for both robots
     # Use "robot" as primary (for compatibility with env_cfg) and "robot2" as secondary
@@ -72,9 +78,10 @@ def create_scene_cfg() -> SceneCfg:
         entities={
             "robot": robot_cfg,
             "robot2": robot2_cfg,
-            "sofa": sofa_cfg,
         },
         sensors=(self_collision_sensor, feet_ground_cfg),
+        # Use spec_fn to add the sofa asset
+        spec_fn=add_sofa_asset,
     )
     
     return scene_cfg
